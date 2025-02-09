05058cb043055b345123f162c2cccee05baeef19
ssokka <ssokka.root@gmail.com>
2025-02-09 21:25:58 +0900

    Update app-clone-user-setting.sh

--- Script/app-clone-user-setting.sh
+++ Script/app-clone-user-setting.sh
@@ -2,7 +2,7 @@
 
 [[ $# != 1 || -z `pm list packages ${1}` ]] && exit -1
 
-aid=$(($(dumpsys package $1 | grep userId | cut -d '=' -f 2)-10000))
+aid=$(($(dumpsys package ${1} | grep userId | cut -d '=' -f 2)-10000))
 dirs=(
 	/data/misc/profiles/cur
 	/data/user
@@ -16,16 +16,23 @@ caches=(
  	code_cache
 )
 
+apid=`pidof ${1}`
+
 for uid in `ls /data/user`; do
-	[[ ${uid} == 0 ]] && continue
+	am force-stop --user ${uid} ${1} -f
+ 	[[ ${uid} == 0 ]] && continue
 	own=u${uid}_a${aid}
 	for dir in ${dirs[@]}; do
 		[[ ! -d ${dir} ]] && continue
-		[[ -d ${dir}/${uid}/$1 ]] && rm -rf ${dir}/${uid}/$1
-		cp -R ${dir}/0/$1 ${dir}/${uid}/
-		chown -R ${own}:${own} ${dir}/${uid}/$1
+		[[ -d ${dir}/${uid}/${1} ]] && rm -rf ${dir}/${uid}/${1}
+		cp -R ${dir}/0/${1} ${dir}/${uid}/
+		chown -R ${own}:${own} ${dir}/${uid}/${1}
 		for cache in ${caches[@]}; do
-			[[ -d ${dir}/${uid}/$1/${cache} ]] && chown -R ${own}:${own}_cache ${dir}/${uid}/$1/${cache}
+			[[ -d ${dir}/${uid}/${1}/${cache} ]] && chown -R ${own}:${own}_cache ${dir}/${uid}/${1}/${cache}
 		done
 	done
+done
+
+for uid in `ls /data/user`; do
+	[[ ${apid} ]] && am start --user ${uid} ${1}
 done
