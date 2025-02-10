b0c84bfcf564a0c4114195fe54000540cca0876d
ssokka <ssokka.root@gmail.com>
2025-02-10 12:39:23 +0900

    Update app-permission.sh

--- Script/app-permission.sh
+++ Script/app-permission.sh
@@ -2,12 +2,15 @@
 
 [[ $# != 1 || -z `pm list packages ${1}` ]] && exit -1
 
+echo -e "\033[32m### 앱 권한 설정\033[m"
 echo
-echo "### $1"
+
+echo -e "\033[33m## ${1}\033[m"
 echo
 
 for uid in `ls /data/user`; do
-	echo "# User ID ${uid}"
+	
+ 	echo "# User ID ${uid}"
  	am start-user ${uid} -W
   	
  	echo " 앱 > 권한 > 위치"
@@ -21,8 +24,8 @@ for uid in `ls /data/user`; do
 	pm grant --user ${uid} ${1} android.permission.WRITE_EXTERNAL_STORAGE 2>/dev/null
  	
   	echo " 앱 > 엡 베터리 사용량 > 제한 없음"
- 	appops set --user ${uid} ${1} RUN_ANY_IN_BACKGROUND allow
-  	appops set --user ${uid} ${1} WAKE_LOCK allow
+ 	appops set --user ${uid} ${1} RUN_ANY_IN_BACKGROUND allow 2>/dev/null
+  	appops set --user ${uid} ${1} WAKE_LOCK allow 2>/dev/null
    	am broadcast --user ${uid} -W -a android.intent.action.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS -d package:${1} 2>/dev/null
 	
  	echo " 앱 > 사용하지 않을 때 앱 활동 일시 중지 > 끔"
@@ -40,33 +43,23 @@ for uid in `ls /data/user`; do
  	echo " 앱 > 특수 앱 액세스 > 사용 기록 액세스 > 허용됨"
 	appops set --user ${uid} ${1} GET_USAGE_STATS allow 2>/dev/null
 
-  	echo
-
-done
-
-echo " 앱 > 특수 앱 액세스 > 배터리 최적화 > 최적화하지 않음(리부팅 시 적용)"
-dumpsys deviceidle whitelist +${1} 2>/dev/null
-
-if [[ ${1} == "net.dinglisch.android.taskerm" ]]; then
-	for uid in `ls /data/user`; do
-
-   		echo "# User ID 0"
-     		
+	if [[ ${1} == "net.dinglisch.android.taskerm" && ${uid} == 0 ]]; then
+		
    		echo " 앱 > 권한 > 추가 권한"
 		#pm grant --user ${uid} com.joaomgcd.autonotification net.dinglisch.android.tasker.PERMISSION_SEND_COMMAND
-		pm grant --user ${uid} net.dinglisch.android.taskerm net.dinglisch.android.zoom.permission.MAKE_CHANGES
-		pm grant --user ${uid} net.dinglisch.android.taskerm com.termux.permission.RUN_COMMAND
+		pm grant --user ${uid} net.dinglisch.android.taskerm net.dinglisch.android.zoom.permission.MAKE_CHANGES 2>/dev/null
+		pm grant --user ${uid} net.dinglisch.android.taskerm com.termux.permission.RUN_COMMAND 2>/dev/null
 		
   		echo " 앱 > 특수 앱 액세스 > 기기 관리자 앱"
-		dpm set-active-admin --user ${uid} net.dinglisch.android.taskerm/.MyDeviceAdminReceiver
+		dpm set-active-admin --user ${uid} net.dinglisch.android.taskerm/.MyDeviceAdminReceiver 2>/dev/null
 		
   		echo "  접근성"
-  		#settings put --user ${uid} secure enabled_accessibility_services net.dinglisch.android.taskerm/net.dinglisch.android.taskerm.MyAccessibilityService:com.joaomgcd.autoinput/com.joaomgcd.autoinput.service.ServiceAccessibilityV2:com.carriez.flutter_hbb/com.carriez.flutter_hbb.InputService:com.joaomgcd.autonotification/com.joaomgcd.autonotification.service.ServiceToastIntercept
-		settings put --user ${uid} secure enabled_accessibility_services net.dinglisch.android.taskerm/net.dinglisch.android.taskerm.MyAccessibilityService:com.joaomgcd.autoinput/com.joaomgcd.autoinput.service.ServiceAccessibilityV2
-
-    		echo
+  		#settings put --user ${uid} secure enabled_accessibility_services net.dinglisch.android.taskerm/net.dinglisch.android.taskerm.MyAccessibilityService:com.joaomgcd.autoinput/com.joaomgcd.autoinput.service.ServiceAccessibilityV2:com.carriez.flutter_hbb/com.carriez.flutter_hbb.InputService:com.joaomgcd.autonotification/com.joaomgcd.autonotification.service.ServiceToastIntercept 2>/dev/null
+		settings put --user ${uid} secure enabled_accessibility_services net.dinglisch.android.taskerm/net.dinglisch.android.taskerm.MyAccessibilityService:com.joaomgcd.autoinput/com.joaomgcd.autoinput.service.ServiceAccessibilityV2 2>/dev/null
 		
-	done
-fi
+	fi
+  	
+done
 
-echo
+echo " 앱 > 앱 배터리 사용량 > 제한 없음"
+dumpsys deviceidle whitelist +${1} 2>/dev/null
